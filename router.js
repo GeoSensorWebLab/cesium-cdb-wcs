b8ef0df8fb926c5ace947c8c9adf9e3e6c1c6f11
James Badger <james@jamesbadger.ca>
2017-08-15 15:12:15 -0600

    Add local proxy to remote WCS
    Because Cesium.js cannot do HTTP Basic authentication in its
    requests, so a local proxy server is used to do it instead.

--- router.js
+++ router.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var path    = require('path');
+var proxy   = require('http-proxy');
+
+var config = require('./config');
+
+var WCSproxy = proxy.createProxyServer({
+  auth: config.wcs.username + ':' + config.wcs.password,
+  changeOrigin: true,
+  secure: true,
+  target: config.wcs.url
+});
 
 // Use express's Router to catch all routes and handle them by sending the
 // index path to the watcher.
 var router = express.Router();
+router.get('/wcs*', function(req, res) {
+  WCSproxy.web(req, res);
+});
+
 router.get('/*', function(req, res, next) {
   // Check for resource requests and redirect to the index file for them
   if (path.extname(req.path).length > 0) {
